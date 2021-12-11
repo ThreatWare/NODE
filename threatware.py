09db5b2026b212d7d26ed7f7954ca9cef8c72ae1
Jonathan Graham <jgraham10@athabasca.edu>
2021-12-11 04:58:19 +0000

    Added documentation to python code

--- threatware.py
+++ threatware.py
@@ -3,16 +3,29 @@ import os,json,subprocess
 import configparser
 from flask import Flask,request,jsonify, redirect, url_for
 from flask_cors import CORS, cross_origin
+
+#Standard Flask startup code
 app = Flask(__name__)
 app.run(ssl_context='adhoc')
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+
+#Read configuration file in .ini format
 config = configparser.ConfigParser()
 config.read('threatware.ini')
+
+#Set config variables
 base = os.path.join(config['general']['rootdirectory'],'modules')
 pythonpath = config['general']['pythonpath']
 apiversion = config['general']['apiversion']
 app.config['UPLOAD_FOLDER'] = config['general']['uploadfolder']
+
+# API Handling Routines
+
+#API Call:
+#listModules - this call lists all available ThreatWare Modules
+#Parameters: None
+#
 @app.route('/%s/modules/list'%apiversion)
 def listModules():
 	if authCheck():
@@ -31,6 +44,11 @@ def listModules():
 		return jsonify({'action':'listModules','result':modules}) 
 	else:
 		return jsonify({'action':'listModules','result':'Unauthorized Access','error':True})
+
+#API Call:
+#getConfig - reads the JSON config file
+#Parameters: None
+#
 @app.route('/%s/modules/<module_name>/config'%apiversion,methods = ['GET'])
 def getConfig(module_name):
 	if authCheck():
@@ -45,6 +63,11 @@ def getConfig(module_name):
 		return jsonify({'action':'getConfig','module':module_name,'result':config})
 	else:
 		return jsonify({'action':'getConfig','result':'Unauthorized Access','error':True})
+
+#API Call:
+#writeConfig - Overwites existing config file with the one passed in the form field "configuration"
+#Parameters: configuration - form field
+#
 @app.route('/%s/modules/<module_name>/config'%apiversion,methods = ['POST'])
 def writeConfig(module_name):
 	if authCheck():
@@ -57,6 +80,11 @@ def writeConfig(module_name):
 		return jsonify({'action':'writeConfig','module':module_name,'result':'OK'})  
 	else:
 		return jsonify({'action':'writeConfig','result':'Unauthorized Access','error':True})
+
+#API Call: 
+#runModule - Executes the run.py script in the designated module directory
+#Parameters: None
+#
 @app.route('/%s/modules/<module_name>/run'%apiversion)
 def runModule(module_name):
 	if authCheck():
@@ -70,6 +98,11 @@ def runModule(module_name):
 		return jsonify({'action':'runModule','module':module_name,'result':output.decode('UTF-8')})
 	else:
 		return jsonify({'action':'runModule','result':'Unauthorized Access','error':True})
+
+#API Call:
+#listResults: Provide a list of all log files in a particular module directory
+#Parameters: None
+#
 @app.route('/%s/modules/<module_name>/results'%apiversion)
 def listResults(module_name):
 	if authCheck():
@@ -81,6 +114,10 @@ def listResults(module_name):
 		return jsonify({'action':'listResults','module':module_name,'result':results})
 	else:
 		return jsonify({'action':'listResults','result':'Unauthorized Access','error':True})
+#API Call:
+#showResults - Fetches a specified log file from the module directory
+#Paramenters: logFile name (without .log) passed on the URL line
+#
 @app.route('/%s/modules/<module_name>/results/<id>'%apiversion)
 def showResults(module_name,id):
 	if authCheck():
@@ -91,6 +128,11 @@ def showResults(module_name,id):
 		return jsonify({'action':'showResult','module':module_name,'result':output})
 	else:
 		return jsonify({'action':'showResult','result':'Unauthorized Access','error':True})
+
+#API Call:
+#installModule - Allows a zipped directory containing a ThreatWare module to be uploaded
+#                via a standard HTTP upload.
+#Paramenters: file - containing the uploaded file.  Upload via HTTP POST.
 @app.route('/%s/system/install/<filename>'%apiversion,methods=['POST'])
 def installModule(filename):
 	if authCheck():
@@ -101,6 +143,9 @@ def installModule(filename):
 		return jsonify({'action':'installModule','module':filename,'result':'OK'})
 	else:
 		return jsonify({'action':'installModule','result':'Unauthorized Access','error':True})
+#Function:
+#authCheck - Utility function used to check that the API is being used by an authorized application.
+#
 def authCheck():
 	headers = request.headers
 	auth = headers.get("user_token")
